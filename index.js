7f40947d8ad8fe7698c2544e80719b7d440a2e3a
Taasin <taasin.saquib@gmail.com>
2018-02-08 16:47:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+
+//get key for mLab
+const keys = require('./config/keys')
+
+// get schema defined in model
+require('./models/db')
+const Person = mongoose.model('Person')
+
+// connect to mLabs database
+mongoose.connect(keys.mongoURI);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+const app = express();
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+	res.send("hello");
+})
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log("listening on port 3000");
+})
