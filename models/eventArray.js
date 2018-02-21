58814c84a76644370157937831d929cd986a4af0
Taasin <taasin.saquib@gmail.com>
2018-02-20 18:45:30 -0800

    fixed eventArray.js

--- models/eventArray.js
+++ models/eventArray.js
@@ -5,7 +5,7 @@ var eventSchema = require('./events');
 
 var eventArr = new Schema({
     events : {type: [eventSchema], required: true},
-    id : {type: number, required: true},
+    id : {type: Number, required: true},
 });
 
 mongoose.model('eventArraySchema', eventArr);
