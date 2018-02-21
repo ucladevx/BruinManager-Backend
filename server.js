40dbd482a95bade4f0e03cde35405c70bcd59d06
Taasin <taasin.saquib@gmail.com>
2018-02-20 18:53:14 -0800

    add route to save mappening data to db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 var cors = require('cors')
 var path = require("path"); // needed?
+const axios = require('axios')
 
 //get key for mLab
 const keys = require('./config/keys');
@@ -13,12 +14,14 @@ require('./models/db');
 require('./models/class');
 require('./models/enrollment');
 
+require("./models/events");
 require("./models/eventArray");
 
 const userSchema = mongoose.model('userSchema');
 const classSchema = mongoose.model('classSchema');
 const enrollmentSchema = mongoose.model('enrollmentSchema');
 
+const eventSchema = mongoose.model('eventSchema');
 const eventArraySchema = mongoose.model('eventArraySchema');
 
 
@@ -33,6 +36,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 
+
+// route to save user data once a quarter or whenever they want to update their classes
 app.post('/user', function(req, res){
 
 	var classes = req.body.classes;
@@ -55,36 +60,70 @@ app.post('/user', function(req, res){
 	res.send("posted");
 });
 
+// return an array of the user's classes
 app.get('/api/classes/:username', function(req, res){
 
 	userSchema.findOne({ "name" : req.params.username}, 'classes', function (err, classArr) {
 	  	if (err) return handleError(err);
 
 		res.send(classArr.classes);
-	  
 	});
 });
 
+// return an array of the user's passes
 app.get('/api/passes/:username', function(req, res){
 
 	userSchema.findOne({ "name" : req.params.username}, 'enrollment', function (err, passArr) {
 	  	if (err) return handleError(err);
 
 	  	res.send(passArr.enrollment);
-		// console.log(passArr.enrollment);
 	});
-
 });
 
+// save 10 Mappening events to db
+app.get('/api/getEvents', function(req, res){
+
+	var eventArr = [];
+
+	axios.get('http://whatsmappening.io/api/v1/events')
+		.then((res) => {
+			for(var i = 0; i < 10; i++){
+				
+				var data = (res.data.features)[i].properties;
+				// console.log(data);
+				var event = new eventSchema({
+				    name : data.event_name,
+				    start_time : data.start_time,
+				    end_time : data.end_time,
+				    location : data.venue.name,
+				    going : data.stats.attending,
+				    interested : data.stats.interested,
+				    picture : data.cover_picture,
+				    category : data.category
+				});
+				eventArr.push(event);
+			}
+
+			// console.log(eventArr);
+			var eventContainer = new eventArraySchema({
+				events: {eventArr},
+				id: 1
+			})
+
+			eventContainer.save();
+
+		})
+		.catch((err) => console.log(err));
+});
+
+// return an array of 10 top events from Mappening API for the day
 app.get('/api/events/:dateID', function(req, res){
 
 	eventArraySchema.findOne({ "id" : req.params.dateID}, 'events', function (err, eventArr) {
 	  	if (err) return handleError(err);
 
 	  	res.send(eventArr);
-		// console.log(passArr.enrollment);
 	});
-
 });
 
 app.get('/', function(req, res) {
