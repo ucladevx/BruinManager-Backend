b442d851f04c764be4981cf232460a5eb8b4a0b5
Amir <saad.amir17@gmail.com>
2018-02-27 12:16:33 -0800

    Dining Hall API

--- server.js
+++ server.js
@@ -88,7 +88,92 @@ app.get('/auth/facebook/callback',
 
 // request a dining hall name, return if its open or not and when it will close if open
 app.get('/api/hours/:diningHall', function(req,res){
-	// TODO: implement
+	//Get the date
+	var d = new Date();
+
+	//Day format: Sunday - Saturday -> 0 - 6
+	var day = d.getDay();
+
+	var options = {
+		uri: 'http://menu.dining.ucla.edu/Hours',
+		transform: function(body) {
+			return cheerio.load(body);
+		}
+	};
+
+	rp(options)
+		.then(($) => {
+			/* Get breakfast hours */
+			//var t = $('tbody').html();
+			//var h = t.children('tr').html();
+
+			var diningHours = {
+				Covel: [],
+				DeNeve: [],
+				FEAST: [],
+				BruinPlate: [],
+				BruinCafe: [],
+				Cafe1919: [],
+				Rendezvous: [],
+				DeNeveGrabNGo: [],
+				TheStudyatHedrick: []
+			};
+
+			var t = $('tbody').find('tr').first('td');
+
+			var dine = [];
+
+			dine[0] = t;
+
+
+			var hoursArray = [];
+
+			for(var i = 1; i < 9; i++) {
+				dine[i] = dine[i - 1].next();
+			}
+
+			
+			//Iterate through each dining hall and add to array
+		
+			for(var i = 0; i < 9; i++) {
+
+				var n =  dine[i].children().first('td');
+
+				var diningName;
+
+				var text;
+
+				//Determine name of dining hall to index the diningHours object
+				switch(i) {
+					case 0: diningName = "Covel"; break;
+					case 1: diningName = "DeNeve"; break;
+					case 2: diningName = "FEAST"; break;
+					case 3: diningName = "BruinPlate"; break;
+					case 4: diningName = "BruinCafe"; break;
+					case 5: diningName = "Cafe1919"; break;
+					case 6: diningName = "Rendezvous"; break;
+					case 7: diningName = "DeNeveGrabNGo"; break;
+					case 8: diningName = "TheStudyatHedrick"; break;
+				}
+
+				for(var j = 0; j < 3; j++) {
+					text = n.next().text();
+					text = text.replace(/(\r\n|\n|\r)/gm,"");
+					text = text.split(' ').join('');
+					diningHours[diningName].push(text);
+					n = n.next();					
+				}
+			}
+
+			var jsonObject = JSON.stringify(diningHours);
+
+			res.send(jsonObject);
+
+
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 // route to save user data once a quarter or whenever they want to update their classes
